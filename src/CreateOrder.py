2edea91d233eeb9bb7ccbe36a1fa46bda7673f7c
Qamar Tanweer <qtanweer.mts41ceme@gmail.com>
2023-04-20 09:39:20 +0500

    added queue address

--- src/CreateOrder.py
+++ src/CreateOrder.py
@@ -2,19 +2,19 @@ import json
 import boto3
 
 sqs = boto3.client('sqs')
-queue_url = 'Placeholder url'
+queue_url = 'https://sqs.ap-southeast-1.amazonaws.com/830103335377/OrderQueue'
 
 
 def lambda_handler(event, context):
-    order = json.loads(event['body'])
+    # order = json.loads(event['body'])
     response = sqs.send_message(
         QueueUrl=queue_url,
-        MessageBody=json.dumps(order)
+        #MessageBody=json.dumps(order)
     )
     print("Creating order..")
     print("sending in Queue..")
 
-    
+
     return {
         'statusCode': 200,
         'body': json.dumps('Order created!')
