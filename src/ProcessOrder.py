1d06e5d290e0c53955d5f3f4066d4b3789d171cd
Qamar Tanweer <qtanweer.mts41ceme@gmail.com>
2023-05-03 19:00:16 +0500

    added SNS for notifications

--- src/ProcessOrder.py
+++ src/ProcessOrder.py
@@ -1,8 +1,34 @@
 import json
+import boto3
+
+sqs = boto3.client('sqs')
+sns = boto3.client('sns')
+queue_url = 'https://sqs.ap-southeast-1.amazonaws.com/830103335377/OrderQueue'
+topic_arn = 'arn:aws:sns:ap-southeast-1:830103335377:OrderTopic'
+
+
 
 def lambda_handler(event, context):
-    print("Processing order..")
+    for record in event['Records']:
+        message = json.loads(record['body'])
+        order_id = message['order_id']
+        customer_name = message['customer_name']
+        order_total = message['order_total']
+
+        subject = f'New Order {order_id}'
+        message = f'Customer Name: {customer_name}\nOrder Total: {order_total}'
+
+        response = sns.publish(
+            TopicArn=topic_arn,
+            Subject=subject,
+            Message=message
+        )
+
+        print(response)
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Order processed!')
+        'body': json.dumps({
+            'message': 'Orders processed'
+        })
     }
